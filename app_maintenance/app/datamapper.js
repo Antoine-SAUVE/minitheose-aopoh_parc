86fcbfd52f689e32e4f902cbd013052f4af5a452
Your NameNeemko <thomascapo31@gmail.com>
2021-10-01 11:42:47 +0200

    ajout sur maintenance

--- app_maintenance/app/datamapper.js
+++ app_maintenance/app/datamapper.js
@@ -6,6 +6,26 @@ const datamapper = {
             text:"SELECT * FROM attraction_with_incident"
         };
         return await pool.query(query);
+    },
+
+    findIncidentById: async (incidentId, callback) => {
+        const query = {
+            text: "SELECT * FROM attraction_with_incident WHERE incidentid=$1",
+            values:[incidentId]
+        };
+        return await pool.query(query, callback);
+    },
+
+    updateIncicent: async (updateId, form) => {
+        const query = {
+            text:`
+                UPDATE attraction_with_incident 
+                SET close_time=$1, description=$2 ,technician=$3, incident_date=$4, resolved_date=$5
+                WHERE incidentId = $6;`,
+            values: [close_time, description, technician, incident_date, resolved_date, updateId]
+        };
+
+        return await pool.query
     }
 };
 
