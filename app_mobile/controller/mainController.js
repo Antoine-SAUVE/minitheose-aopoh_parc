25f6bebb76d8b37353636a61b7c54b7cb5a88e78
Your NameNeemko <thomascapo31@gmail.com>
2021-10-01 16:53:43 +0200

    ajout des modifications app mobile

--- app_mobile/controller/mainController.js
+++ app_mobile/controller/mainController.js
@@ -1,21 +1,31 @@
 const dataMapper = require("../datamaper");
 
+
 const mainController = {
+
+     visitorId : 1,
+
     init:async(req,res)=>{
      
         const result = await dataMapper.init(req.body);
         console.log(result.rows[0]);
         res.json(result.rows[0]);
+        visitorId = (result.rows[0].visitorid);
+        console.log('visitorData:'+visitorId)
     },
 
 
 
     events:async(req,res)=>{
         let result = await dataMapper.events();
-        console.log(result.rows);
-
-            
+        console.log(result.rows);     
         res.json(result.rows);
+    },
+
+    bookings:async(req,res)=>{
+        let result = await dataMapper.bookings(visitorId);
+        console.log(result.rows[0]);
+        res.json(result.rows[0]);
     }
 };
 
